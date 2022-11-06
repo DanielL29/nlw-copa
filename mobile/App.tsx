84c678d508de5bb95a131af7ad47978a78b2c874
DanielL29 <danielnatsufire@gmail.com>
2022-11-06 20:37:20 -0300

    feat: add types and axios instance

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -14,6 +14,7 @@ import { AuthContextProvider } from "./src/contexts/AuthContext";
 import { New } from "./src/screens/New";
 import { Pools } from "./src/screens/Pools";
 import { Find } from "./src/screens/Find";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +32,7 @@ export default function App() {
           translucent
         />
 
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
